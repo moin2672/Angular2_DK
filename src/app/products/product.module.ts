cef4f63138bb8104edb45c125058760c3152c0bd
moin2672 <moin2672@gmail.com>
2018-05-10 19:22:35 +0530

    L11b

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './product-detail.component';
-import { StarComponent } from '../shared/star.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipes';
 import { ProductListComponent } from './product-list.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductGuardService } from './product-guard.service';
 import { ProductService } from './product.service';
@@ -12,8 +8,6 @@ import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {
@@ -26,8 +20,6 @@ import { SharedModule } from './../shared/shared.module';
   ],
   declarations: [
     ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
     ProductDetailComponent
   ],
   providers: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { StarComponent } from "./star.component";
+import { FormsModule } from "@angular/forms";
+import { ConvertToSpacesPipe } from "./convert-to-spaces.pipes";
 
 @NgModule({
-  imports: [
-    CommonModule
+  imports: [CommonModule],
+  declarations: [
+    StarComponent,
+    ConvertToSpacesPipe
   ],
-  declarations: []
+  exports: [
+    StarComponent,
+    ConvertToSpacesPipe,
+    CommonModule,
+    FormsModule
+  ]
 })
-export class SharedModule { }
+export class SharedModule {}
